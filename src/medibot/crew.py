368240b26e2081199c9f763d2da26f307cc047d7
Tram Mai <maihoangbichtram@gmail.com>
2024-11-28 08:46:26 +0200

    Edit task of GP

--- src/medibot/crew.py
+++ src/medibot/crew.py
@@ -1,12 +1,17 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-from crewai_tools import SerperDevTool, ScrapeWebsiteTool
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, VisionTool
 from pydantic import BaseModel, Field
 from typing import List, Optional
-from medibot.tools.custom_tool import HumanTool, AppointmentTool
+from medibot.tools.custom_tool import HumanTool, AppointmentTool, EyeTool
 from pydantic import BaseModel, Field
 from crewai.tasks.task_output import TaskOutput
 from crewai.tasks.conditional_task import ConditionalTask
+import requests
+import json
+
+# Loading Human Tools
+#human_tools = load_tools(["human"])
 
 # Define a condition function for the conditional task
 # If false, the task will be skipped, if true, then execute the task.
@@ -16,8 +21,10 @@ def is_doctor_checkup_needed(output: TaskOutput) -> bool:
 
 class Preassessment(BaseModel):
     """General practitioner Preassessment output model"""
-    message_to_patient: str = Field(..., description="Comprehensive message to the patient")
-    doctor_checkup_needed: bool = Field(..., description="whether the patient to see the doctor urgently")
+    #message_to_patient: str = Field(..., description="Comprehensive message to the patient")
+    interview_summary: str = Field(..., description="the detailed summary of the patient's input")
+    diagnoses: str = Field(..., description="The diagnoses based on your medical knowledge")
+    #doctor_checkup_needed: bool = Field(..., description="whether the patient to see the doctor urgently")
 
 def general_practitioner_task_callback(output: TaskOutput):
     # Do something after the task is completed
@@ -26,6 +33,17 @@ def general_practitioner_task_callback(output: TaskOutput):
     print(output)
     print('###Output raw###')
     print(output.raw)
+    print(type(output.raw))
+    # print(output.interview_summary)
+    #print(output.raw.interview_summary)
+    #print(json.loads(output.raw).get('interview_summary'))
+    print(output.pydantic.interview_summary)
+
+    data = {'patient_id': '160900AAgent', 'description': str(output.pydantic.interview_summary), 'pre_assessment': str(output.pydantic.diagnoses)}
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.post('https://main-bvxea6i-5gtrd4rbck7xm.eu-5.platformsh.site/add-request', data=json.dumps(data), headers=headers)
+    print(response.text)
 
 @CrewBase
 class MedibotCrew():
@@ -36,40 +54,52 @@ class MedibotCrew():
     def general_practitioner(self) -> Agent:
         return Agent(
             config=self.agents_config['general_practitioner'],
+            #tools=[SerperDevTool(), VisionTool(), HumanTool(), EyeTool()], # Example of custom tool, loaded at the beginning of file
             tools=[SerperDevTool(), HumanTool()], # Example of custom tool, loaded at the beginning of file
             verbose=True,
             allow_delegation=False,
         )
 
-    @agent
-    def clinic_appointment_maker(self) -> Agent:
+    '''@agent
+    def clinic_receptionist(self) -> Agent:
         return Agent(
-            config=self.agents_config['clinic_appointment_maker'],
-            tools=[AppointmentTool()],
+            config=self.agents_config['clinic_receptionist'],
+            #tools=[EyeTool(), AppointmentTool(), VisionTool()],
+            tools=[EyeTool()],
             verbose=True,
             allow_delegation=False,
-        )
+        )'''
+
+    '''@task
+    def personal_info_collector(self) -> Task:
+        return Task(
+            config=self.tasks_config['personal_info_collector'],
+            agent=self.clinic_receptionist(),
+            #callback=general_practitioner_task_callback
+
+            # condition=is_doctor_checkup_needed
+        )'''
 
     @task
     def general_practitioner_task(self) -> Task:
         return Task(
             config=self.tasks_config['general_practitioner_task'],
             agent=self.general_practitioner(),
-            #output_pydantic=Preassessment,
-            callback=general_practitioner_task_callback
+            output_pydantic=Preassessment,
+            callback=general_practitioner_task_callback,
             # human_input=True
         )
 
-    @task
-    def clinic_appointment_maker_task(self) -> Task:
-        return Task(
+    '''@task
+    def clinic_appointment_maker_task(self) -> ConditionalTask:
+        return ConditionalTask(
             config=self.tasks_config['clinic_appointment_maker_task'],
-            agent=self.clinic_appointment_maker(),
+            agent=self.clinic_receptionist(),
             context=[self.general_practitioner_task()],
-            callback=general_practitioner_task_callback
+            #callback=general_practitioner_task_callback
 
-            # condition=is_doctor_checkup_needed
-        )
+            condition=is_doctor_checkup_needed
+        )'''
 
     @crew
     def crew(self) -> Crew:
--- src/medibot/tools/custom_tool.py
+++ src/medibot/tools/custom_tool.py
@@ -1,5 +1,10 @@
 from crewai_tools import BaseTool
-
+import cv2
+from openai import OpenAI
+import base64
+import time
+from ultralytics import YOLO
+import math
 
 class HumanTool(BaseTool):
     name: str = "Human interact"
@@ -24,3 +29,83 @@ class AppointmentTool(BaseTool):
         if string: print(string)
         if argument: res = input(f"{argument} \n")
         return res
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+def on_camera():
+    frameWidth = 640
+    frameHeight = 480
+    cap = cv2.VideoCapture(0)
+    cap.set(3, frameWidth)
+    cap.set(4, frameHeight)
+    cap.set(10, 150)
+    # client = OpenAI()
+
+    model = YOLO("yolov8n-wounds.pt")  # load a pretrained model (recommended for training)
+    # model = YOLO("yolo-Weights/yolov8n.pt")
+    classNames = ["wound"]
+    i = 1
+    time = 1
+    imgs = []
+
+    while i <= 15 and time <= 600:
+        success, img = cap.read()
+        results = model(img, stream=True)
+
+        # coordinates
+        for r in results:
+            boxes = r.boxes
+
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+
+                # confidence
+                confidence = math.ceil((box.conf[0] * 100)) / 100
+                print("Confidence --->", confidence)
+
+                # class name
+                cls = int(box.cls[0])
+                print("Class name -->", classNames[cls])
+
+                if (confidence > 0.7):
+                    # put box in cam
+                    print(x1, y1, x2, y2)
+                    cropped = img[y1:y2, x1:x2]
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                    i += 1
+                    # object details
+                    org = [x1, y1]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    color = (255, 0, 0)
+                    thickness = 1
+
+                    cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                    cv2.imwrite(f"GeeksForGeeks_{i}.png", img)
+                    cv2.imwrite(f"GeeksForGeeks_{i}_cropped.png", cropped)
+                    imgs.append(f"GeeksForGeeks_{i}_cropped.png")
+        cv2.imshow('Webcam', img)
+        time += 1
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    print("image saved.")
+    return imgs
+
+class EyeTool(BaseTool):
+    name: str = "Human eyes"
+    description: str = (
+        "Capture some images about open wounds."
+    )
+
+    def _run(self, argument: str) -> str:
+        return on_camera()
+        # return ''
